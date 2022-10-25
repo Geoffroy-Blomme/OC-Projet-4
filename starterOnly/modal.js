b157b41b1c2314b50f5f6dc407037bb739e51148
unknown <geoffroy.blomme@gmail.com>
2022-10-25 16:47:46 +0200

    added verifications for all the fields, form-sent now has a max-width and its top margin is handled in the JS

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -15,15 +15,32 @@ const modalCloseBtn = document.querySelector(".close");
 const submitBtn = document.querySelector(".btn-submit");
 const modalForm = document.querySelector(".modal-body form");
 const formSent =document.querySelector(".form-sent");
+const formSentText = document.querySelector(".form-sent__text");
 const formSentCloseButton = document.querySelector(".btn-submit--close");
 const firstName = document.querySelector("#first");
 const lastName = document.querySelector("#last");
+const email = document.querySelector("#email");
+const dateOfBirth = document.querySelector("#birthdate");
+const quantity = document.querySelector("#quantity");
+const locationBtns = document.querySelectorAll(".location-checkbot");
+const checkBoxTermsOfUse = document.querySelector("#checkbox1");
+const allInputs = document.querySelectorAll(".formData input");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-//close modal event 
+// Close modal event
 modalCloseBtn.addEventListener("click",closeModal);
+formSentCloseButton.addEventListener("click",closeModal);
+
+// prevents the box to appear when the input is invalid
+allInputs.forEach(element => {
+  element.addEventListener( "invalid",
+  function( event ) {
+      event.preventDefault();
+  });
+});
+
 
 // launch modal form
 function launchModal() {
@@ -53,7 +70,6 @@ function checkIfNameValid(name,errorMsg){
   
 }
 
-
 // return true and call the checkSuceeded function if email is valid,  returns false and call the checkFailed functionif it isn't
 function checkEmail(email){
   let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -65,6 +81,58 @@ function checkEmail(email){
   // If the email is incorrect
   else{
     checkFailed(email,"Veuillez indiquer une addresse mail correcte");
+    return false;
+  }
+}
+
+// return true and call the checkSuceeded function if the date of birth is correct, returns false and call the checkFailed function if is incorrect
+function checkDateOfBirth(dateOfBirth){
+  let reg = /\d{4}-\d{1,2}-\d{1,2}/;
+  // xxxx-xx-xx, xxxx-x-xx, xxxx-xx-x and xxxx-x-x will be accepted with x being a number from 0 to 9
+  if(reg.test(dateOfBirth.value)){
+    checkSucceeded(dateOfBirth);
+    return true;
+  }
+  else{
+    checkFailed(dateOfBirth,"Veuillez indiquer votre date de naissance.");
+    return false;
+  }
+}
+
+
+// return true and call the checkSuceeded function if quantity is a number,  returns false and call the checkFailed function  if it isn't
+function checkIfNumber(quantity){
+  if((!isNaN(quantity.value)) && (quantity.value!=="") && (quantity.value <= 99) && (quantity.value >= 0)){
+      checkSucceeded(quantity);
+      return true;
+  }
+  else{
+    checkFailed(quantity,"Veuillez indiquer un nombre compris entre 0 et 99");
+    return false;
+  }
+}
+
+// returns true and call the checkSuceeded function if at least one checkbox is checked,  returns false and call the checkFailed function if none is checked
+function checkIfAtLeastOneCheck(inputs){
+  for(let i = 0; i < inputs.length; i++){
+    if(inputs[i].checked){
+      checkSucceeded(inputs[0]);
+      return true;
+    }
+
+  }
+  checkFailed(inputs[0],"Veuillez choisir une location");
+  return false;
+}
+
+// return true and call the checkSuceeded function if the terms of use's checkbox is checked, returns false otherwise.
+function termsOfUseAccepted(checkBoxTermsOfUse){
+  if(checkBoxTermsOfUse.checked){
+    checkSucceeded(checkBoxTermsOfUse);
+    return true;
+  }
+  else{
+    checkFailed(checkBoxTermsOfUse,"Veuillez accepter les conditions d'utilisation");
     return false;
   }
 }
@@ -86,7 +154,11 @@ function checkSucceeded(element){
 function checkIfAllGood(){
   return  checkIfNameValid(firstName,"Votre Prénom doit contenir au moins deux lettres")
   & checkIfNameValid(lastName,"Votre Nom doit contenir au moins deux lettres")
-  & checkEmail(email);
+  & checkEmail(email)
+  & checkDateOfBirth(dateOfBirth)
+  & checkIfNumber(quantity)
+  & checkIfAtLeastOneCheck(locationBtns)
+  & termsOfUseAccepted(checkBoxTermsOfUse);
 }
 
 // Changes the content of the modal to show the content when the form is sent.
@@ -99,6 +171,12 @@ function modalChangeContent(){
   formSent.style.width = modalFormWidth + "px";
   formSent.style.height = modalFormHeight + "px";
   formSent.style.display="flex";
+  // Si on reduit la resolution apres avoir donner une largeur fixe a formSent, on veut que celui-ci ne depasse pas
+  formSent.style.maxWidth = "100%";
+  
+  formSentText.style.marginTop = (modalFormHeight/2) + "px";
+
+
 }
 
 // If the data of the form are correct, 
